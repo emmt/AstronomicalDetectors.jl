b5cb683746bbcd9f4f44b062f510488e3157ee8d
buthanoid <portail@buthanoid.eu>
2023-03-30 15:01:47 +0200

    replaced tabs by spaces

--- src/ReadCalibration.jl
+++ src/ReadCalibration.jl
@@ -7,7 +7,7 @@ using ScientificDetectors:CalibrationFrameSampler
 import ScientificDetectors.Calibration: prunecalibration
 
 """
-	fill_filedict!(filedict,catdict,dir)
+    fill_filedict!(filedict,catdict,dir)
 
 fill dictionary `filedict` with fitsheader of all files in `dir` according to the keywords in `catdict`
 The dictionary  `filedict` key is the filepath.
@@ -32,105 +32,105 @@ function fill_filedict!(filedict::Dict{String, FitsHeader},
 end
 
 function fill_filedict!(filedict::Dict{String, FitsHeader},
-						catdict::Dict{String, Any},
-						dirs::Vector{String})
-	for dir in dirs
-		fill_filedict!(filedict,catdict,dir)
-	end
+                        catdict::Dict{String, Any},
+                        dirs::Vector{String})
+    for dir in dirs
+        fill_filedict!(filedict,catdict,dir)
+    end
 end
 
 
 """
-	contains(chain::Union{AbstractString,Vector{AbstractString}}, pattern::Vector{AbstractString})
+    contains(chain::Union{AbstractString,Vector{AbstractString}}, pattern::Vector{AbstractString})
 
 Overloading of Base.contains for vectors of string. Return `true` if a `chain`
 contains one of the patterns given in `pattern`
 """
 function Base.contains(chain::Union{String,Vector{String}}, pattern::Vector{String})
-	for str in pattern
-		if contains(chain,str)
-			return true
-		end
-	end
-	return false
+    for str in pattern
+        if contains(chain,str)
+            return true
+        end
+    end
+    return false
 end
 
 """
-	contains(chain::Vector{String}, pattern::String)
+    contains(chain::Vector{String}, pattern::String)
 
 Overloading of Base.contains for vectors of string. Return `true` if one of the `chains`
 contains `pattern`
 """
 function Base.contains(chains::Vector{String}, pattern::AbstractString)
-	for chain in chains
-		if contains(chain,pattern)
-			return true
-		end
-	end
-	return false
+    for chain in chains
+        if contains(chain,pattern)
+            return true
+        end
+    end
+    return false
 end
 
 
 
 """
-	endswith(chain::Union{String,Vector{String}}, pattern::Vector{String})
+    endswith(chain::Union{String,Vector{String}}, pattern::Vector{String})
 
 Overloading of Base.endswith for vectors of string. Return `true` if `chain`
 ends with one of the patterns given in `pattern`
 """
 function Base.endswith(chain::Union{String,Vector{String}}, pattern::Vector{String})
-	for str in pattern
-		if endswith(chain,str)
-			return true
-		end
-	end
-	return false
+    for str in pattern
+        if endswith(chain,str)
+            return true
+        end
+    end
+    return false
 end
 
 """
-	endswith(chain::Vector{String}, pattern::String)
+    endswith(chain::Vector{String}, pattern::String)
 
 Overloading of Base.endswith for vectors of string. Return `true` if `chain`
 ends with `pattern`
 """
 function Base.endswith(chains::Vector{String},pattern::AbstractString)
-	for chain in chains
-		if endswith(chain,pattern)
-			return true
-		end
-	end
-	return false
+    for chain in chains
+        if endswith(chain,pattern)
+            return true
+        end
+    end
+    return false
 end
 
 
 """
-	filterfilename(filelist,name)
+    filterfilename(filelist,name)
 
 Remove all files from `filelist` that do not match the strings in `name`.
 """
 function filterfilename(filelist::Dict{String, FitsHeader},name::String)
-	return filterfilename(filelist,[name])
+    return filterfilename(filelist,[name])
 end
 
 function filterfilename(filelist::Dict{String, FitsHeader},name::Vector{String})
-	pattern =Regex("("*join(name,")|(")*")")
-	return filter(p->match(pattern,p.first) !== nothing,filelist)
+    pattern =Regex("("*join(name,")|(")*")")
+    return filter(p->match(pattern,p.first) !== nothing,filelist)
 end
 
 
 """
-	newlist = filtercat(filelist,keyword,value)
+    newlist = filtercat(filelist,keyword,value)
 
 Build a `newlist` dictionnary of all files where `fitsheader[keyword].value() == value`.
 """
 function filtercat(filelist::Dict{String, FitsHeader},
-					keyword::String,
-					values::Union{Vector{String}, Vector{Bool}, Vector{Integer}, Vector{AbstractFloat}})
-	newlist = Dict{String, FitsHeader}()
-	for value in values
-		merge!(newlist, filtercat(filelist,keyword,value))
-	end
-	return newlist
+                    keyword::String,
+                    values::Union{Vector{String}, Vector{Bool}, Vector{Integer}, Vector{AbstractFloat}})
+    newlist = Dict{String, FitsHeader}()
+    for value in values
+        merge!(newlist, filtercat(filelist,keyword,value))
+    end
+    return newlist
 end
 
 function filtercat(filelist::Dict{String, FitsHeader},
@@ -143,42 +143,42 @@ function filtercat(filelist::Dict{String, FitsHeader},
 end
 
 """
-	newlist = filtercat(filelist::Dict{String, FitsHeader},catdict::Dict{String, Any})
+    newlist = filtercat(filelist::Dict{String, FitsHeader},catdict::Dict{String, Any})
 
 Build a `newlist` dictionnary of all files where `fitsheader[keyword] == value` for all keywords contained in `catdict`
 """
 function  filterkeyword(filelist::Dict{String, FitsHeader},
-						catdict::Dict{String, Any})
-	filteredkeywords = "(dir)|(files)|(suffixes)|(include subdirectory)|(exclude files)|(exptime)|(hdu)|(sources)|(roi)"
-	keydict =  filter(p->match(Regex(filteredkeywords), p.first) === nothing,catdict)
-	if length(keydict)>0
-		for (keyword,value) in keydict
-			filelist =  filtercat(filelist,keyword,value)
-		end
-	end
-	return filelist
+                        catdict::Dict{String, Any})
+    filteredkeywords = "(dir)|(files)|(suffixes)|(include subdirectory)|(exclude files)|(exptime)|(hdu)|(sources)|(roi)"
+    keydict =  filter(p->match(Regex(filteredkeywords), p.first) === nothing,catdict)
+    if length(keydict)>0
+        for (keyword,value) in keydict
+            filelist =  filtercat(filelist,keyword,value)
+        end
+    end
+    return filelist
 end
 
 function default_calibdict(dir::AbstractString,roi)
-	calibdict = Dict{String, Any}()
-	calibdict["dir"] = dir
-	calibdict["hdu"] = 1
-	calibdict["suffixes"] = [".fits", ".fits.gz","fits.Z"]
-	calibdict["include subdirectory"] = true
-	calibdict["exclude files"] = Vector{String}()
-	calibdict["roi"] = roi
-	return calibdict
+    calibdict = Dict{String, Any}()
+    calibdict["dir"] = dir
+    calibdict["hdu"] = 1
+    calibdict["suffixes"] = [".fits", ".fits.gz","fits.Z"]
+    calibdict["include subdirectory"] = true
+    calibdict["exclude files"] = Vector{String}()
+    calibdict["roi"] = roi
+    return calibdict
 end
 
 function default_category_dict(calibdict::Dict{String, Any})
-	filteredkeywords = "(categories)|(title)";
- 	catdict  = Dict{String, Any}()
-	merge!(catdict,filter(p->match(Regex(filteredkeywords), p.first) === nothing,calibdict));
-	return catdict
+    filteredkeywords = "(categories)|(title)";
+    catdict  = Dict{String, Any}()
+    merge!(catdict,filter(p->match(Regex(filteredkeywords), p.first) === nothing,calibdict));
+    return catdict
 end
 
 """
-	ReadCalibrationFiles(yaml_file::AbstractString; roi::NTuple{2} = (:,:),  dir=pwd())
+    ReadCalibrationFiles(yaml_file::AbstractString; roi::NTuple{2} = (:,:),  dir=pwd())
 
 Process calibration files according to the YAML configuration file `yaml_file`.
 
@@ -192,65 +192,65 @@ Return an instance of `CalibrationData` with all information statistics needed t
 """
 function ReadCalibrationFiles(yaml_file::AbstractString; roi = (:,:),  dir = pwd(), prune=true)
 
-	calibdict = default_calibdict(dir,repr(roi))
-	#merge!(calibdict,vararg)
-	merge!(calibdict,YAML.load_file(yaml_file; dicttype=Dict{String,Any}))
+    calibdict = default_calibdict(dir,repr(roi))
+    #merge!(calibdict,vararg)
+    merge!(calibdict,YAML.load_file(yaml_file; dicttype=Dict{String,Any}))
 
-	filedict = Dict{String, FitsHeader}()
+    filedict = Dict{String, FitsHeader}()
 
-	catarr =  [CalibrationCategory(cata,Meta.parse(value["sources"])) for (cata,value) in calibdict["categories"] ]
-	local caldat::CalibrationData{Float64}
-	local dataroi::DetectorAxes
-	local inds::Tuple{OrdinalRange, OrdinalRange}
-	isfirst = true
-	width, height = -1, -1
+    catarr =  [CalibrationCategory(cata,Meta.parse(value["sources"])) for (cata,value) in calibdict["categories"] ]
+    local caldat::CalibrationData{Float64}
+    local dataroi::DetectorAxes
+    local inds::Tuple{OrdinalRange, OrdinalRange}
+    isfirst = true
+    width, height = -1, -1
 
-	for (cat,value) in calibdict["categories"]
-		catdict =default_category_dict(calibdict)
-		merge!(catdict, value)
-		fill_filedict!(filedict,calibdict,catdict["dir"])
-		haskey(catdict,"files") && fill_filedict!(filedict,calibdict,catdict["files"])
-		filescat = filterkeyword(filedict,catdict)
-		if !isempty(filescat)
-			for (filename,fitshead) in filescat
+    for (cat,value) in calibdict["categories"]
+        catdict =default_category_dict(calibdict)
+        merge!(catdict, value)
+        fill_filedict!(filedict,calibdict,catdict["dir"])
+        haskey(catdict,"files") && fill_filedict!(filedict,calibdict,catdict["files"])
+        filescat = filterkeyword(filedict,catdict)
+        if !isempty(filescat)
+            for (filename,fitshead) in filescat
 
-				FitsFile(filename) do file
+                FitsFile(filename) do file
 
-				    hdu = FitsImageHDU(file, Int(catdict["hdu"]))
+                    hdu = FitsImageHDU(file, Int(catdict["hdu"]))
 
-				    if isfirst
-					    width, height = hdu.data_size
-					    inds = (Base.OneTo(width)[eval(Meta.parse(catdict["roi"]))[1]],
-					    Base.OneTo(height)[eval(Meta.parse(catdict["roi"]))[2]])
-					    dataroi = DetectorAxes(inds)
-					    caldat = CalibrationData{Float64}(dataroi,catarr)
-					    isfirst = false
-				    else
-					    width  == hdu.data_size[1] || error("incompatible sizes")
-					    height == hdu.data_size[2] || error("incompatible sizes")
-				    end
-				    if hdu.data_ndims == 2
-					    data = read(hdu, inds...)
-					    sampler =  CalibrationDataFrame(cat,fitshead[catdict["exptime"]].float,data;roi=dataroi)
-				    else
-					    data = read(hdu, (inds...,Base.OneTo(hdu.data_size[3]))...)
+                    if isfirst
+                        width, height = hdu.data_size
+                        inds = (Base.OneTo(width)[eval(Meta.parse(catdict["roi"]))[1]],
+                        Base.OneTo(height)[eval(Meta.parse(catdict["roi"]))[2]])
+                        dataroi = DetectorAxes(inds)
+                        caldat = CalibrationData{Float64}(dataroi,catarr)
+                        isfirst = false
+                    else
+                        width  == hdu.data_size[1] || error("incompatible sizes")
+                        height == hdu.data_size[2] || error("incompatible sizes")
+                    end
+                    if hdu.data_ndims == 2
+                        data = read(hdu, inds...)
+                        sampler =  CalibrationDataFrame(cat,fitshead[catdict["exptime"]].float,data;roi=dataroi)
+                    else
+                        data = read(hdu, (inds...,Base.OneTo(hdu.data_size[3]))...)
 
-					    if hdu.data_size[3] > 1
-						    sampler = CalibrationFrameSampler(data,cat,fitshead[catdict["exptime"]].float;roi=dataroi)
-					    else
-						    sampler =  CalibrationDataFrame(cat,fitshead[catdict["exptime"]].float,view(data, :,:, 1);roi=dataroi)
-					    end
-				    end
-				    push!(caldat, sampler)
-				end
-			end
-		end
-	end
-	if prune
-		caldat = prunecalibration(caldat)
-	end 
+                        if hdu.data_size[3] > 1
+                            sampler = CalibrationFrameSampler(data,cat,fitshead[catdict["exptime"]].float;roi=dataroi)
+                        else
+                            sampler =  CalibrationDataFrame(cat,fitshead[catdict["exptime"]].float,view(data, :,:, 1);roi=dataroi)
+                        end
+                    end
+                    push!(caldat, sampler)
+                end
+            end
+        end
+    end
+    if prune
+        caldat = prunecalibration(caldat)
+    end
 
-	return caldat
+    return caldat
 end
 
 end
